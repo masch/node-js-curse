580a74956ef73e4dbffc132ca0e580183ad97189
Nahuel <ncaceres@andreani.com>
2023-08-16 08:16:23 -0300

    add: middlewares

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const ditto = require("./pokemon/ditto.json");
+
+const PORT = process.env.PORT ?? 1234;
+
 const app = express();
 
 app.disable("x-powered-by");
 
-const PORT = process.env.PORT ?? 1234;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hola viejo</h1>");
-});
-
-app.post("/pokemon", (req, res) => {
+/*
+app.use((req, res, next) => {
+  if (req.method !== "POST") return next();
+  if (req.headers["content-type"] !== "application/json") return next();
+  console.log("arranca el post");
+  // solo llegan requests que son POST y su header tiene Content-Type: application/json
   let body = "";
 
   req.on("data", (chuck) => {
@@ -20,13 +25,34 @@ app.post("/pokemon", (req, res) => {
     const data = JSON.parse(body);
     data.timestamp = Date.now();
 
-    console.log("recurso creado", { data });
-    res.status(201).json(data);
+    console.log("test post");
+    // mutar la request y guardarla en el req.body
+    req.body = data;
+    next();
   });
+});
+*/
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hola viejo</h1>");
+});
+
+app.get("/pokemon/ditto", (req, res) => {
+  res.json(ditto);
+});
+
+app.post("/pokemon", (req, res) => {
+  req.body.timestamp = Date.now();
+
+  return res.status(201).json(req.body);
+});
+
+app.use((req, res) => {
+  res.status(404).send("<h1>404</h1>");
 });
 
 app.listen(PORT, () => {
   console.log(`servidor escuchando en puerto ${PORT}`);
 });
 
-//https://www.youtube.com/watch?v=YmZE1HXjpd4&t=1604s
+// https://www.youtube.com/watch?v=-9d3KhCqOtU&list=PLUofhDIg_38qm2oPOV-IRTTEKyrVBBaU7&index=3
